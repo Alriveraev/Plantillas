95da9585a3cf8f7d905044d2d71f79c4ace8b5fd
Alriveraev <aerivera@innovacion.gob.sv>
2026-04-21 15:17:43 -0600

    Borrado de cache

---
+++ .net-plantilla_capas/BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,49 @@
+﻿using BlogCoreSolution.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var lista = _contenedorTrabajo.Articulo.GetAll();
+            return Json(new { data = lista });
+        }
+
+
+        //[HttpDelete]
+        //public IActionResult Delete(int id)
+        //{
+        //    var categoria = _contenedorTrabajo.Categoria.Get(id);
+        //    if (categoria == null)
+        //    {
+        //        return Json(new { success = false, message = "Error al eliminar la categoría" });
+        //    }
+
+        //    _contenedorTrabajo.Categoria.Remove(categoria);
+        //    _contenedorTrabajo.Save();
+        //    return Json(new { success = true, message = "Categoría eliminada correctamente" });
+        //}
+        #endregion
+    }
+}
