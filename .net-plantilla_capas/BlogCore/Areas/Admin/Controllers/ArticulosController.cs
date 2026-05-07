6646e4f9a14422427174348a6ea603c12c2c665c
Alriveraev <aerivera@innovacion.gob.sv>
2026-05-07 15:51:21 -0600

    Add image upload and markdown editor to articles
    Enabled image upload for articles and integrated SimpleMDE markdown editor in Create and Edit views. Updated views for improved UX and registered image folder in project file.

--- .net-plantilla_capas/BlogCore/Areas/Admin/Controllers/ArticulosController.cs
+++ .net-plantilla_capas/BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -1,6 +1,7 @@
 ﻿using BlogCoreSolution.DataAccess.Data.Repository.IRepository;
 using BlogCoreSolution.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BlogCore.Areas.Admin.Controllers
 {
@@ -8,10 +9,11 @@ namespace BlogCore.Areas.Admin.Controllers
     public class ArticulosController : Controller
     {
         private readonly IContenedorTrabajo _contenedorTrabajo;
-
-        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo, IWebHostEnvironment hostingEnvironment)
         {
             _contenedorTrabajo = contenedorTrabajo;
+            _hostingEnvironment = hostingEnvironment;
         }
 
 
@@ -33,8 +35,57 @@ namespace BlogCore.Areas.Admin.Controllers
             return View(articuloVM);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ArticuloVM artiVm)
+        {
+
+            if (!ModelState.IsValid) 
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+
+                if(artiVm.Articulo.Id == 0 && archivos.Count > 0) 
+                {
+                    //Nuevo articulo
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+                    }
+                    artiVm.Articulo.UrlImagen = @"\imagenes\articulos\" + nombreArchivo + extension;
+                    _contenedorTrabajo.Articulo.Add(artiVm.Articulo);
+                    _contenedorTrabajo.Save();
+
+                    return RedirectToAction(nameof(Index));
+                } else {
+                    ModelState.AddModelError("Imagen", "Seleccione una imagen para el artículo.");
+                }
+
+            }
+
+            artiVm.ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias();
+            return View(artiVm);
+        }
 
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            ArticuloVM articuloVM = new ArticuloVM()
+            {
+                Articulo = new BlogCoreSolution.Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias(),
+            };
+
+            if(id != null) {
+                articuloVM.Articulo = _contenedorTrabajo.Articulo.Get(id.GetValueOrDefault());
+            }
+
+            return View(articuloVM);
+        }
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
